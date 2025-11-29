84ba88e99006b185759b690b18aa3c1805a24b0b
Shashwati-2002-git <shashwati2023@gmail.com>
2025-11-29 19:51:14 +0530

    Add CORS middleware to server

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ const path = require("path");
 const axios = require("axios");
 const cheerio = require("cheerio");
 const OpenAI = require("openai");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 
